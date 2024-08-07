88a3540db9215096fedc08af85224c19c0436888
e.larroche <e.larroche@lockself.com>
2024-08-07 23:55:57 +0200

    popup target wip & wip constellation svg

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,8 +1,14 @@
 import type { PopupProps } from "./types"
 
+import Techno from "../../assets/icons/techno.svg?react"
+import Cyber from "../../assets/icons/cyber-secu.svg?react"
+import Leader from "../../assets/icons/leader.svg?react"
+import Archi from "../../assets/icons/archiWeb.svg?react"
+
 import "./popup.scss"
 
 const Popup = ({
+	target,
 	size,
 	stopPropagation,
 	onMouseEnter,
@@ -14,7 +20,44 @@ const Popup = ({
 			onClick={stopPropagation}
 			onMouseEnter={onMouseEnter}
 			onMouseLeave={onMouseLeave}
-		></div>
+		>
+			{target === "withMe" && (
+				<div className="popupContainerContent">
+					<div className="contentLeft">
+						<div className="title">Use cases</div>
+						<ul>
+							<li>
+								<div className="iconUseCase">
+									<Techno />
+								</div>
+								<div className="textUp">lorem ipsum justo</div>
+							</li>
+							<li>
+								<div className="iconUseCase">
+									<Cyber />
+								</div>
+								<div className="textUp">lorem ipsum justo</div>
+							</li>
+							<li>
+								<div className="iconUseCase">
+									<Leader />
+								</div>
+								<div className="textUp">lorem ipsum justo</div>
+							</li>
+							<li>
+								<div className="iconUseCase">
+									<Archi />
+								</div>
+								<div className="textUp">lorem ipsum justo</div>
+							</li>
+						</ul>
+					</div>
+					<div className="contentRight">
+						<div className="title">User storie</div>
+					</div>
+				</div>
+			)}
+		</div>
 	)
 }
 
--- src/pages/leftSide/index.tsx
+++ src/pages/leftSide/index.tsx
@@ -2,12 +2,17 @@ import Title from "../../components/title/title"
 import NavBar from "../../components/navBar/navBar"
 import CopyRight from "../../components/copyRight"
 
+import ConstellationSVG from "../../assets/icons/constellation.svg?react"
+
 import "./index.scss"
 
 const LeftSide = () => {
 	return (
 		<div className="leftPart">
 			<NavBar />
+			<div className="constellation">
+				<ConstellationSVG />
+			</div>
 			<Title />
 			<CopyRight />
 		</div>
