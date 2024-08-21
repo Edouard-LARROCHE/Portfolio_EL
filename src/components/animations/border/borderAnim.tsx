46e2b9d776fa27f22eb44c83460704dea78de8e9
e.larroche <e.larroche@lockself.com>
2024-08-21 22:03:15 +0200

    update animate border

--- src/components/animations/border/borderAnim.tsx
+++ src/components/animations/border/borderAnim.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useRef } from "react"
+
 import "./borderAnim.scss"
 
 const BorderAnim = () => {
+	const animRef = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (animRef.current) {
+				const elements = animRef.current.querySelectorAll(".span")
+
+				elements.forEach((element) => {
+					element.classList.remove("animated")
+				})
+
+				void animRef.current.offsetWidth
+
+				elements.forEach((element) => {
+					element.classList.add("animated")
+				})
+			}
+		}, 12000)
+
+		return () => clearInterval(interval)
+	}, [])
+
 	return (
-		<>
-			<div className="span"></div>
-			<div className="span"></div>
-			<div className="span"></div>
-			<div className="span"></div>
-		</>
+		<div ref={animRef}>
+			<div className="span animated"></div>
+			<div className="span animated"></div>
+			<div className="span animated"></div>
+			<div className="span animated"></div>
+		</div>
 	)
 }
 
