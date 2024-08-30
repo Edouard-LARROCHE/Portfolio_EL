861d0199fac9ee533905a8f472fc4babae0beb8a
e.larroche <e.larroche@lockself.com>
2024-08-30 22:23:36 +0200

    component screen size down

---
+++ src/components/animations/loader/loaderBar/loaderBar.tsx
@@ -0,0 +1,8 @@
+import "./loaderBar.scss"
+
+const LoaderBar = () => {
+	return <div className="loaderBar" />
+}
+
+export default LoaderBar
+
--- src/pages/screenSizeWarning/screenSizeWarning.tsx
+++ src/pages/screenSizeWarning/screenSizeWarning.tsx
@@ -1,10 +1,19 @@
+import LoaderBar from "../../components/animations/loader/loaderBar/loaderBar"
+
+import ConstellationSVG from "../../assets/icons/constellation.svg?react"
+
 import "./screenSizeWarning.scss"
 
 const ScreenSizeWarning = () => {
 	return (
 		<div className="screenSizeWarning">
-			<div className="text">
-				Under construction... Please return to a suitable screen.
+			<div className="text">Under construction...</div>
+			<LoaderBar />
+			<div className="extendText">
+				Please return to a suitable screen.
+			</div>
+			<div className="constellation">
+				<ConstellationSVG />
 			</div>
 		</div>
 	)
